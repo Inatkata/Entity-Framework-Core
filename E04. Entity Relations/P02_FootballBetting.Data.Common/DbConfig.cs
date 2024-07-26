acd276a1d50fda1abaaa9c5bdb0da60eba591382
Ina Kavalova <135525228+Inatkata@users.noreply.github.com>
2024-07-26 13:18:04 +0300

    Add files via upload

---
+++ E04. Entity Relations/P02_FootballBetting.Data.Common/DbConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace P02_FootballBetting.Data.Common
+{
+    public static class DbConfig
+    {
+        // ReSharper disable once InconsistentNaming
+        public const string ConnectionString =
+            @"Server=MACBOOKC58F\SQLEXPRESS;Database=Bet388;Integrated Security=True;Encrypt=False;";
+    }
+}
